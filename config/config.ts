4a15d34a822ba3e4286f69afafc95d3f109f097f
Cristofer Llovera <cristoferllovera43@gmail.com>
2023-06-24 06:32:02 -0400

    project start

---
+++ config/config.ts
@@ -0,0 +1,31 @@
+import * as dotenv from "dotenv";
+
+export abstract class ConfigServer {
+  constructor() {
+    const nodeNameEnv = this.createPathEnv(this.nodeEnv);
+    dotenv.config({
+      path: nodeNameEnv,
+    });
+  }
+
+  public getEnviromet(k: string): string | undefined {
+    return process.env[k];
+  }
+
+  public getNumberEnv(k: string): number {
+    return Number(this.getEnviromet(k));
+  }
+
+  public get nodeEnv(): string {
+    return this.getEnviromet("NODE_ENV")?.trim() || "";
+  }
+
+  public createPathEnv(path: string): string {
+    const arrEnv: Array<string> = ["env"];
+    if (path.length > 0) {
+      const stringToArray = path.split(".");
+      arrEnv.unshift(...stringToArray);
+    }
+    return "." + arrEnv.join(".");
+  }
+}
